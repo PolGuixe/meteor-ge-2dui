d2a80e16ee7c158c95457574490644430bcda02b
PolGuixe <pol.guixe@gmail.com>
2016-01-12 00:06:10 +0100

    update git reference

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'polguixe:meteor-ge-2dui',
   version: '4.4.3',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor wrapper for FIWARE GE 2D-UI',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/PolGuixe/meteor-ge-2dui',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
